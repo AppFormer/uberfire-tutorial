a523f749a2ee7b104f6d7b59987ff208dbe6d212
Jonathan Fuerth <jfuerth@gmail.com>
2013-12-16 18:27:21 -0500

    Checkpoint 2 - Add a second screen
    Conflicts:
    	src/main/java/com/mycompany/uftutorial/client/HomePerspective.java

--- src/main/java/com/mycompany/uftutorial/client/HelloWorldScreen.java
+++ src/main/java/com/mycompany/uftutorial/client/HelloWorldScreen.java
@@ -1,6 +1,7 @@
 package com.mycompany.uftutorial.client;
 
 import javax.enterprise.context.Dependent;
+import javax.enterprise.event.Observes;
 
 import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
@@ -8,6 +9,7 @@ import org.uberfire.client.annotations.WorkbenchScreen;
 
 import com.google.gwt.user.client.ui.IsWidget;
 import com.google.gwt.user.client.ui.Label;
+import com.mycompany.uftutorial.shared.Mood;
 
 @Dependent
 @WorkbenchScreen(identifier = "com.mycompany.uftutorial.client.HelloWorldScreen")
@@ -25,5 +27,9 @@ public class HelloWorldScreen {
   @WorkbenchPartView
   public IsWidget getView() {
     return label;
+  }
+
+  public void onMoodChange(@Observes Mood mood) {
+    label.setText("I understand you are feeling " + mood.getText());
   }
 }
--- src/main/java/com/mycompany/uftutorial/client/HomePerspective.java
+++ src/main/java/com/mycompany/uftutorial/client/HomePerspective.java
@@ -7,6 +7,8 @@ import org.uberfire.client.annotations.WorkbenchPerspective;
 import org.uberfire.mvp.impl.DefaultPlaceRequest;
 import org.uberfire.workbench.model.PanelType;
 import org.uberfire.workbench.model.PerspectiveDefinition;
+import org.uberfire.workbench.model.Position;
+import org.uberfire.workbench.model.impl.PanelDefinitionImpl;
 import org.uberfire.workbench.model.impl.PartDefinitionImpl;
 import org.uberfire.workbench.model.impl.PerspectiveDefinitionImpl;
 
@@ -21,9 +23,17 @@ public class HomePerspective {
     final PerspectiveDefinition p = new PerspectiveDefinitionImpl(PanelType.ROOT_LIST);
     p.setTransient(true);
     p.setName(getClass().getName());
+
     p.getRoot().addPart(
             new PartDefinitionImpl(
                     new DefaultPlaceRequest(HelloWorldScreen.class.getName())));
+
+    PanelDefinitionImpl westPanel = new PanelDefinitionImpl(PanelType.MULTI_LIST);
+    p.getRoot().insertChild(Position.WEST, westPanel);
+    westPanel.setWidth(250);
+    westPanel.addPart(
+            new PartDefinitionImpl(
+                    new DefaultPlaceRequest(MoodScreen.class.getName())));
 
     return p;
   }
