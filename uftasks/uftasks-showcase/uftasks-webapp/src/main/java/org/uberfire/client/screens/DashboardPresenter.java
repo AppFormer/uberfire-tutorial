8899d52a4bb5b9fbed02f205959f0e667bdef41e
Paulo Martins <paulovmr@gmail.com>
2015-12-15 18:36:26 -0200

    Changing uberfire archetype version to 0.7.4.Final.

--- uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/DashboardPresenter.java
+++ uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/DashboardPresenter.java
@@ -1,10 +1,20 @@
-package org.uberfire.client.screens;
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import java.util.HashMap;
-import java.util.Map;
-import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.event.Observes;
-import javax.inject.Inject;
+package org.uberfire.client.screens;
 
 import org.uberfire.client.annotations.WorkbenchPartTitle;
 import org.uberfire.client.annotations.WorkbenchPartView;
@@ -13,6 +23,12 @@ import org.uberfire.client.mvp.UberView;
 import org.uberfire.lifecycle.OnOpen;
 import org.uberfire.shared.events.TaskCreated;
 import org.uberfire.shared.events.TaskDone;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.inject.Inject;
+import java.util.HashMap;
+import java.util.Map;
 
 @ApplicationScoped
 @WorkbenchScreen(identifier = "DashboardPresenter")
--- uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/ProjectsView.java
+++ uftasks/uftasks-showcase/uftasks-webapp/src/main/java/org/uberfire/client/screens/ProjectsView.java
@@ -1,19 +1,34 @@
-package org.uberfire.client.screens;
+/*
+ * Copyright 2015 Red Hat, Inc. and/or its affiliates.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import javax.annotation.PostConstruct;
-import javax.enterprise.context.Dependent;
+package org.uberfire.client.screens;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import org.gwtbootstrap3.client.ui.Button;
 import org.gwtbootstrap3.client.ui.LinkedGroup;
 import org.gwtbootstrap3.client.ui.LinkedGroupItem;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.Dependent;
 
 @Dependent
 public class ProjectsView extends Composite implements ProjectsPresenter.View {
